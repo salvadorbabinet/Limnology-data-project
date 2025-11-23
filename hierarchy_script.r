a0ee9448d7fde188c85f9f0af92496e9cea59463
salvadorbabinet <salvador.babinet@mail.mcgill.ca>
2025-11-23 14:51:45 -0500

    Can constrain a gam to get significant relationship over 7 cuts

--- hierarchy_script.r
+++ hierarchy_script.r
@@ -41,13 +41,13 @@ gam1 <- gam(log_zoo_density ~ log_chla, data = plankton)
 gam2 <- gam(log_zoo_density ~ s(log_chla), data = plankton)
 
 summary(gam1)
-summary(gam2)
+summary(gam2) # Significantly linear, though low R2
 
 gam1 <- gam(log_zoo_density ~ log_invivo_chla, data = plankton)
 gam2 <- gam(log_zoo_density ~ s(log_invivo_chla), data = plankton)
 
 summary(gam1)
-summary(gam2) # lower R2 than filtered measure
+summary(gam2) # Even lower R2 than filtered measure
 
 
 # Take temperature ntiles and nest regressions.
@@ -63,6 +63,12 @@ ntiles_estimates |> arrange(desc(term)) |> print(n = 20)
 lm7 <- lm(estimate ~ cut_midtemp, data = filter(ntiles_estimates, term == "Effect"))
 summary(lm7)
 
+gam7 <- gam(estimate ~ s(cut_midtemp, k = 3), data = filter(ntiles_estimates, term == "Effect"))
+summary(gam7)
+
+plot(gam7)
+gam.check(gam7)
+
 # For 14 ntiles:
 ntiles <- ncut_temperature(14, plankton)
 ntiles_estimates <- nest_temperature(cut_midtemp, ntiles)
@@ -70,8 +76,8 @@ ntiles_estimates <- nest_temperature(cut_midtemp, ntiles)
 lm14 <- lm(estimate ~ cut_midtemp, data = filter(ntiles_estimates, term == "Effect"))
 summary(lm14)
 
-gam14 <- gam(estimate ~ s(cut_midtemp), method = "REML", data = filter(ntiles_estimates, term == "Effect"))
 gam14 <- gam(estimate ~ s(cut_midtemp), data = filter(ntiles_estimates, term == "Effect"))
+gam14 <- gam(estimate ~ s(cut_midtemp, k = 5), method = "REML", data = filter(ntiles_estimates, term == "Effect"))
 summary(gam14)
 
 lm14_intercepts <- lm(estimate ~ cut_midtemp, data = filter(ntiles_estimates, term == "Intercept"))
@@ -84,9 +90,11 @@ plot_nest(cut_midtemp, ntiles_estimates, plot_fits = TRUE)
 plot(gam14_intercepts, xlab = "Temperature (°C)", ylab = "Smoothed intercept estimate")
 plot(gam14, xlab = "Temperature (°C)", ylab = "Smoothed effect estimate")
 
+gam.check(gam14)
 
-# Could fit polynomial multiple regression instead of GAM
-# Which is perhaps more sound with so few observations...
+
+# Could fit polynomial regression instead of GAM, which
+# is perhaps more sound with so few observations... ----
 
 summary(lm14)
 
@@ -94,7 +102,7 @@ lm_quad <- lm(estimate ~ cut_midtemp + I(cut_midtemp^2), data = filter(ntiles_es
 summary(lm_quad)
 
 lm_cubic <- lm(estimate ~ cut_midtemp + I(cut_midtemp^2) + I(cut_midtemp^3), data = filter(ntiles_estimates, term == "Effect"))
-summary(lm_cubic)
+summary(lm_cubic) # Final model for effect estimates (on n = 7, n = 14).
 
 ggplot(
     filter(ntiles_estimates, term == "Effect"),
@@ -102,6 +110,7 @@ ggplot(
     ) +
     geom_point(aes(fill = cut_midtemp), shape = 21, size = 4) +
     geom_line(aes(y = fitted(lm14)), color = "black", linetype = 2) +
+    #geom_line(aes(y = fitted(gam7))) +
     geom_line(aes(y = fitted(lm_cubic)), color = "red") +
     scale_fill_gradient2(
         low = "blue", mid = "orange", high = "red",
@@ -115,7 +124,7 @@ ggplot(
 summary(lm14_intercepts)
 
 lm_quadintercepts <- lm(estimate ~ cut_midtemp + I(cut_midtemp^2), data = filter(ntiles_estimates, term == "Intercept"))
-summary(lm_quadintercepts)
+summary(lm_quadintercepts) # Final model for intercept estimates.
 
 lm_cubicintercepts <- lm(estimate ~ cut_midtemp + I(cut_midtemp^2) + I(cut_midtemp^3), data = filter(ntiles_estimates, term == "Intercept"))
 summary(lm_cubicintercepts)
